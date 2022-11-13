533701356153f9a5ec7a1efe0ccc416e4503e478
Ingo Speckens <doningo@ymail.com>
2022-11-13 12:17:44 +0100

    Improve_button_component

--- tiro-client/src/App.tsx
+++ tiro-client/src/App.tsx
@@ -7,9 +7,8 @@ function App() {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       Hallo Tori-User
       <Button
-        styling={
-          "text-base bg-red-300 border-black border hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-gray-400"
-        }
+        bg="bg-red-300"
+        bgHover="hover:bg-red-500"
         children="Absenden"
         onClick={() => console.log("You clicked on the pink circle!")}
       />
--- tiro-client/src/components/Button.tsx
+++ tiro-client/src/components/Button.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 interface Props {
-  styling: string;
+  bg: string;
+  bgHover: string;
   onClick: () => void;
   children?: React.ReactNode;
 }
 
-const Button = ({ styling, onClick, children }: Props) => {
+const Button = ({ bg, bgHover, onClick, children }: Props) => {
   return (
-    <button onClick={onClick} className={styling}>
+    <button
+      onClick={onClick}
+      className={`pl-2 pr-2 text-base ${bg} border-black border-2 rounded-md ${bgHover} focus:outline-none focus:ring-2 focus:ring-gray-400`}
+    >
       {children}
     </button>
   );
