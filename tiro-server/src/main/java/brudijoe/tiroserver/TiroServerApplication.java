6948195daf89227bc10e5816ffa4d60e1adaac66
Ingo Speckens <doningo@ymail.com>
2022-11-29 19:54:27 +0100

    Add_backend

---
+++ tiro-server/src/main/java/brudijoe/tiroserver/TiroServerApplication.java
@@ -0,0 +1,13 @@
+package brudijoe.tiroserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiroServerApplication.class, args);
+	}
+
+}
---
+++ tiro-server/src/test/java/brudijoe/tiroserver/TiroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package brudijoe.tiroserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiroServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
