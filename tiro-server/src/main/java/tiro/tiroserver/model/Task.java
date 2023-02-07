828ead92927ce644934b8fc8adc9a58e0e2ea445
Ingo Speckens <doningo@ymail.com>
2023-02-07 19:08:01 +0100

    Add_task

---
+++ tiro-server/src/main/java/tiro/tiroserver/model/Task.java
@@ -0,0 +1,29 @@
+package main.java.tiro.tiroserver.model;
+
+public class Task {
+
+    private String taskId;
+    private String taskName;
+
+    public Task(String taskId, String taskName) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+    }
+
+    public String getTaskId() {
+        return this.taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+}
